fdd84d55fdbbd7a9f549f4d673d646c96e95cff1
Igal Tsoiref <itsoiref@redhat.com>
2026-01-21 20:51:19 -0500

    cni, node: Always include bridge name in OVS interface operations and extend test coverage
    - Replace hardcoded 'br-int' with util.GetOvnBridgeName() in CNI helper
    - Add external_ids:bridge-name when creating OVS ports
    - Use configurable bridge name in del-port operations

--- go-controller/pkg/cni/helper_linux.go
+++ go-controller/pkg/cni/helper_linux.go
@@ -565,7 +565,7 @@ func ConfigureOVS(ctx context.Context, namespace, podName, podIfName, hostIfaceN
 			// br-int for the same pod; do not delete port in this case.
 			continue
 		}
-		if out, err := ovsExec("--with-iface", "del-port", "br-int", name); err != nil {
+		if out, err := ovsExec("--with-iface", "del-port", util.GetOvnBridgeName(), name); err != nil {
 			klog.Warningf("Failed to delete stale OVS port %q with iface-id %q from br-int: %v\n %q",
 				name, ifaceID, err, out)
 		}
@@ -592,12 +592,13 @@ func ConfigureOVS(ctx context.Context, namespace, podName, podIfName, hostIfaceN
 	// Add the new sandbox's OVS port, tag the port as transient so stale
 	// pod ports are scrubbed on hard reboot
 	ovsArgs := []string{
-		"--may-exist", "add-port", "br-int", hostIfaceName, "other_config:transient=true",
+		"--may-exist", "add-port", util.GetOvnBridgeName(), hostIfaceName, "other_config:transient=true",
 		"--", "set", "interface", hostIfaceName,
 		fmt.Sprintf("external_ids:attached_mac=%s", ifInfo.MAC),
 		fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
 		fmt.Sprintf("external_ids:iface-id-ver=%s", initialPodUID),
 		fmt.Sprintf("external_ids:sandbox=%s", sandboxID),
+		fmt.Sprintf("external_ids:bridge-name=%s", util.GetOvnBridgeName()),
 	}
 
 	// pod interface name, used to identify CNI request with the same NAD
@@ -920,7 +921,7 @@ func (pr *PodRequest) deletePodConntrack() {
 func (pr *PodRequest) deletePort(ifaceName, podNamespace, podName string) {
 	podDesc := fmt.Sprintf("%s/%s", podNamespace, podName)
 
-	out, err := ovsExec("del-port", "br-int", ifaceName)
+	out, err := ovsExec("del-port", util.GetOvnBridgeName(), ifaceName)
 	if err != nil && !strings.Contains(err.Error(), "no port named") {
 		// DEL should be idempotent; don't return an error just log it
 		klog.Warningf("Failed to delete pod %q OVS port %s: %v\n  %q", podDesc, ifaceName, err, string(out))
